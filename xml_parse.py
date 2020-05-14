105eb1bd6a4cdf6e1a878939041b21925e12db41
Menachem Geva <me.geva@gmail.com>
2020-05-15 02:00:50 +0300

    v1

---
+++ xml_parse.py
@@ -0,0 +1,33 @@
+import xml.etree.ElementTree as ET
+
+my_xml='''<?xml version="1.0" encoding="UTF-8" ?>
+<log>
+ <entry id="1">
+ <message>Application started</message>
+ </entry>
+ <entry id="2">
+ <message>Application ended</message>
+ </entry>
+ <entry id="3">
+ <message>Application ended</message>
+ </entry>
+ <entry id="4">
+ <message>Application ended</message>
+ </entry>
+ <entry id="5">
+ <message>Application ended</message>
+ </entry>
+</log>
+'''
+
+root = ET.fromstring(my_xml)
+
+def ids_by_message(root,str_to_search):
+    l_to_ret=[]
+    for ent in root.findall('./entry'):
+        if ent.find('./message').text==str_to_search:
+            l_to_ret.append(ent.get('id'))
+    return l_to_ret
+
+if __name__=='__main__':
+    print(ids_by_message(root,'Application started g'))
